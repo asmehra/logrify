080ece9324a00d1154ad85560aede8d40e12a328
Aman <amansingh.webdeveloper@gmail.com>
2023-05-06 23:50:05 +0530

    Updated the readme and made dynamically messages

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # Logrify
+
+Logrify is a small utility library for printing console logs in a colorful and easy-to-read format and also print the timestamp of logs. It provides four methods for logging different types of messages: `Log.success()`, `Log.error()`, `Log.warning()`, and `Log.info()`.
+
+## Installation
+
+To use **Logrify** in your project, simply install it via npm:
+
+```sh
+npm install logrify
+```
+
+## Usage
+
+To use **Logrify** in your project, you need to import it first:
+
+```js
+import Log from 'logrify';
+```
+
+After importing Logrify, you can use the following methods to log messages in different colors and styles:
+
+### Log.success()
+
+Use `Log.success()` to log success messages in green color with a timestamp:
+
+```js
+Log.success('Data saved successfully!');
+```
+
+Output:
+
+![enter image description here](https://logrify.files.wordpress.com/2023/05/log.success.png)
+
+### Log.error()
+
+Use `Log.error()` to log error messages in red color with a timestamp:
+
+```js
+Log.error('Something went wrong!');
+```
+
+Output:
+
+![enter image description here](https://logrify.files.wordpress.com/2023/05/log.error_.png)
+
+### Log.warning()
+
+Use `Log.warning()` to log warning messages in yellow color with a timestamp:
+
+```js
+Log.warning('This action cannot be undone!');
+```
+
+Output:
+
+![enter image description here](https://logrify.files.wordpress.com/2023/05/log.warning.png)
+
+### Log. info()
+
+Use `Log.info()` to log informative messages in blue color with a timestamp:
+
+```js
+Log.info('A new library has been released.');
+```
+
+Output:
+
+![enter image description here](https://logrify.files.wordpress.com/2023/05/log.info_.png)
+
+## Contributing
+
+If you find any issues or have suggestions for improvements, please open an issue or pull request on the [Logrify GitHub repository](https://github.com/asmehra/logrify). We welcome contributions from the community!
+
+## License
+
+Logrify is released under the [MIT License](https://opensource.org/licenses/MIT).
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,27 @@
 export default class Log {
     static timestampFormat: Object = { hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit' };
 
-    static log(msg: string) {
-        const date = new Date();
-        const now = date.toLocaleTimeString([], Log.timestampFormat);
-        console.log(`[${now}] ${msg}`);
-    }
-
     static success(msg: string) {
         const date = new Date();
         const now = date.toLocaleTimeString([], Log.timestampFormat);
-        console.log(`%c ${now} %c ${String.fromCharCode(187)} %cHello`, "color:white; background:green; border-radius:2px", "color:whiite", "color:green")
+        console.log(`%c ${now} %c ${String.fromCharCode(187)} %c${msg}`, "color:white; background:green; border-radius:2px", "color:whiite", "color:green")
     }
 
     static error(msg: string) {
         const date = new Date();
         const now = date.toLocaleTimeString([], Log.timestampFormat);
-        console.log(`%c ${now} %c ${String.fromCharCode(187)} %cError`, "color:white; background:red; border-radius:2px", "color:whiite", "color:red")
+        console.log(`%c ${now} %c ${String.fromCharCode(187)} %c${msg}`, "color:white; background:red; border-radius:2px", "color:whiite", "color:red")
     }
 
     static warning(msg: string) {
         const date = new Date();
         const now = date.toLocaleTimeString([], Log.timestampFormat);
-        console.log(`%c ${now} %c ${String.fromCharCode(187)} %cWarn`, "color:black; background:yellow; border-radius:2px", "color:whiite", "color:yellow")
+        console.log(`%c ${now} %c ${String.fromCharCode(187)} %c${msg}`, "color:black; background:yellow; border-radius:2px", "color:whiite", "color:yellow")
     }
 
     static info(msg: string) {
         const date = new Date();
         const now = date.toLocaleTimeString([], Log.timestampFormat);
-        console.log(`%c ${now} %c ${String.fromCharCode(187)} %cWarn`, "color:white; background:blue; border-radius:2px", "color:whiite", "color:blue")
+        console.log(`%c ${now} %c ${String.fromCharCode(187)} %c${msg}`, "color:white; background:blue; border-radius:2px", "color:whiite", "color:blue")
     }
 }
